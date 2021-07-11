2fa993e27b0ac4c34fdcc9122b918d39ab402095
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-07-11 13:31:06 -0300

    completed ong route

--- controllers/ong/index.go
+++ controllers/ong/index.go
@@ -1,7 +1,15 @@
 package ong
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/GoLearningCode/BeTheHeroBackend/database"
+	"github.com/GoLearningCode/BeTheHeroBackend/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Index(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"hello": "world"})
+	db := database.ConnectToDb()
+	db.Table("ong")
+	ongs := []models.Ong{}
+	db.Find(&ongs)
+	return c.JSON(map[string][]models.Ong{"ongs": ongs})
 }
